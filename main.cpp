0f7867b504f871a0978c1eb52d6f270b0eb026ac
Iago Alves <iagoalves.voador@gmail.com>
2023-10-06 16:08:55 -0300

    Encontrando caminhos por BelmannFord

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <climits> 
 
 using namespace std;
 
@@ -35,12 +36,48 @@ public:
       vilas.push_back(vila);
     }
   }
+
+  void atualizarNumeroEstradas() {
+    _numero_estradas = _numero_estradas * 2;
+  }
 };
 
 
 
-void BelmannFord(vector<Estrada> caminho, int origem) {
+void Verifica(int dist[], int n) {
+  cout << "Vila   Distancia do Palacio" << endl;
+  for (int i = 0; i < n; ++i)
+    cout << i + 1 << "\t\t" << dist[i] << endl;
+}
 
+
+
+void BelmannFord(Balconia balconia) {
+  int num_vilas = balconia._numero_vilas;
+  int num_estradas = balconia._numero_estradas;
+
+  int distancias[num_vilas];
+
+  for (int i = 0; i < num_vilas; i++) {
+    distancias[i] = INT_MAX;
+  }
+  distancias[0] = 0;
+
+  for (int i = 1; i <= num_vilas - 1; i++) {
+    for (int j = 0; j < num_estradas; j++) {
+      int u = balconia.estradas[j]._origem._id - 1;
+      int v = balconia.estradas[j]._destino._id - 1;
+
+      int tempo = balconia.estradas[j]._tempo;
+      if (distancias[u] != INT_MAX && distancias[u] + tempo < distancias[v]) {
+        distancias[v] = distancias[u] + tempo;
+      }
+    }
+  }
+
+  Verifica(distancias, num_vilas);
+ 
+  return;
 }
 
 
@@ -67,15 +104,28 @@ int main() {
     int ano, tempo, custo;
     cin >> ano >> tempo >> custo;
 
-    Estrada e;
-    e._origem = origem;
-    e._destino = destino;
-    e._ano = ano;
-    e._tempo = tempo;
-    e._custo = custo;
+    Estrada ida;
+    ida._origem = origem;
+    ida._destino = destino;
+    ida._ano = ano;
+    ida._tempo = tempo;
+    ida._custo = custo;
 
-    balconia.estradas.push_back(e);
+    balconia.estradas.push_back(ida);
+
+    Estrada volta;
+    volta._origem = destino;
+    volta._destino = origem;
+    volta._ano = ano;
+    volta._tempo = tempo;
+    volta._custo = custo;
+
+    balconia.estradas.push_back(volta);
   }
+
+  balconia.atualizarNumeroEstradas();
+
+  BelmannFord(balconia);
 
   return 0;
 }
