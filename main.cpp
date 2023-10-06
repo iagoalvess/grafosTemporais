e37693243a5117dc58eba3941ee0cdbaafdf6763
Iago Alves <iagoalves.voador@gmail.com>
2023-10-06 15:20:39 -0300

    Classes

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ public:
 
 class Estrada {
 public:
-  Vila _inicio;
+  Vila _origem;
   Vila _destino;
 
   int _ano, _tempo, _custo;
@@ -20,34 +20,61 @@ public:
 
 
 
-int main () {
-  int N, M;
+class Balconia {
+public:
+  int _numero_vilas;
+  int _numero_estradas;
 
+  vector<Vila> vilas;
   vector<Estrada> estradas;
 
+  void preencherVilas() {
+    for (int i = 1; i <= _numero_vilas; i++) {
+      Vila vila;
+      vila._id = i;
+      vilas.push_back(vila);
+    }
+  }
+};
+
+
+
+void BelmannFord(vector<Estrada> caminho, int origem) {
+
+}
+
+
+
+int main() {
+  int N, M;
   cin >> N >> M;
+
+  Balconia balconia;
+  balconia._numero_vilas = N;
+  balconia._numero_estradas = M;
+  balconia.preencherVilas();
 
   for (int i = 0; i < M; i++) {
     int id1, id2;
     cin >> id1 >> id2;
 
-    Vila inicio;
+    Vila origem;
     Vila destino;
 
-    inicio._id = id1;
+    origem._id = id1;
     destino._id = id2;
 
     int ano, tempo, custo;
     cin >> ano >> tempo >> custo;
 
     Estrada e;
-    e._inicio = inicio;
+    e._origem = origem;
     e._destino = destino;
     e._ano = ano;
     e._tempo = tempo;
     e._custo = custo;
 
-    estradas.push_back(e);
+    balconia.estradas.push_back(e);
   }
 
   return 0;
