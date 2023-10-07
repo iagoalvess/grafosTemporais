e5209a5a8b2b51eb7096daa0773a65fa4e6fd1fc
Iago Alves <iagoalves.voador@gmail.com>
2023-10-07 18:16:38 -0300

    Kruskal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <climits> 
+#include <climits>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,6 +16,8 @@ class Estrada {
 public:
   Vila _origem;
   Vila _destino;
+
+  bool _usada;
 
   int _ano, _tempo, _custo;
 };
@@ -36,19 +39,88 @@ public:
       vilas.push_back(vila);
     }
   }
+};
 
-  void atualizarNumeroEstradas() {
-    _numero_estradas = _numero_estradas * 2;
+
+
+class UnionFind {
+private:
+  vector<int> pai;
+  vector<int> rank;
+
+public:
+  UnionFind(int n) {
+    pai.resize(n, -1);
+    rank.resize(n, 1);
+  }
+
+  int find(int i) {
+    if (pai[i] == -1)
+      return i;
+
+    return pai[i] = find(pai[i]);
+  }
+
+  void unite(int x, int y) {
+    int s1 = find(x);
+    int s2 = find(y);
+
+    if (s1 != s2) {
+      if (rank[s1] < rank[s2]) {
+        pai[s1] = s2;
+      }
+      else if (rank[s1] > rank[s2]) {
+        pai[s2] = s1;
+      }
+      else {
+        pai[s2] = s1;
+        rank[s1] += 1;
+      }
+    }
   }
 };
 
 
 
-void Verifica(int dist[], int n) {
-  cout << "Vila   Distancia do Palacio" << endl;
-  for (int i = 0; i < n; ++i)
-    cout << i + 1 << "\t\t" << dist[i] << endl;
-}
+void Kruskal(Balconia balconia, function<bool(const Estrada&, const Estrada&)> comparacao, bool tipo) {
+  sort(balconia.estradas.begin(), balconia.estradas.end(), comparacao);
+
+  UnionFind U(balconia._numero_vilas);
+
+  int ans = 0;
+  int ano_maximo = INT_MIN;
+
+  for (auto e : balconia.estradas) {
+    int w = e._custo;
+    int x = e._origem._id - 1;
+    int y = e._destino._id - 1;
+
+    if (U.find(x) != U.find(y)) {
+      U.unite(x, y);
+
+      if (tipo) {
+        ans += e._custo;
+      } else if (e._ano > ano_maximo) {
+        ano_maximo = e._ano;
+      }
+    }
+    else if (U.find(y) != U.find(x)) {
+      U.unite(y, x);
+
+      if (tipo) {
+        ans += e._custo;
+      } else if (e._ano > ano_maximo) {
+        ano_maximo = e._ano;
+      }
+    }
+  }
+
+  if (tipo) {
+    cout << "N+3: " << ans << endl; 
+  } else {
+    cout << "N+2: " << ano_maximo << endl; 
+  }
+} 
 
 
 
@@ -57,6 +129,7 @@ void BelmannFord(Balconia balconia) {
   int num_estradas = balconia._numero_estradas;
 
   int distancias[num_vilas];
+  Estrada* estrada_max_ano = nullptr;  // Para armazenar a estrada com o ano máximo.
 
   for (int i = 0; i < num_vilas; i++) {
     distancias[i] = INT_MAX;
@@ -71,14 +144,38 @@ void BelmannFord(Balconia balconia) {
       int tempo = balconia.estradas[j]._tempo;
       if (distancias[u] != INT_MAX && distancias[u] + tempo < distancias[v]) {
         distancias[v] = distancias[u] + tempo;
+
+        // Marcar a estrada como usada no caminho mínimo.
+        balconia.estradas[j]._usada = true;
+
+        // Verificar se essa é a estrada com o ano máximo até agora.
+        if (!estrada_max_ano || balconia.estradas[j]._ano > estrada_max_ano->_ano) {
+          estrada_max_ano = &balconia.estradas[j];
+        }
+      }
+
+      if (distancias[v] != INT_MAX && distancias[v] + tempo < distancias[u]) {
+        distancias[u] = distancias[v] + tempo;
+
+        // Marcar a estrada como usada no caminho mínimo.
+        balconia.estradas[j]._usada = true;
+
+        // Verificar se essa é a estrada com o ano máximo até agora.
+        if (!estrada_max_ano || balconia.estradas[j]._ano > estrada_max_ano->_ano) {
+          estrada_max_ano = &balconia.estradas[j];
+        }
       }
     }
   }
 
-  Verifica(distancias, num_vilas);
- 
+  for (int i = 0; i < num_vilas; ++i) {
+    cout << distancias[i] << endl;
+  }
+  cout << "N+1: " << estrada_max_ano->_ano << endl;
+
   return;
 }
+
 
 
 
@@ -104,28 +201,20 @@ int main() {
     int ano, tempo, custo;
     cin >> ano >> tempo >> custo;
 
-    Estrada ida;
-    ida._origem = origem;
-    ida._destino = destino;
-    ida._ano = ano;
-    ida._tempo = tempo;
-    ida._custo = custo;
+    Estrada e;
+    e._origem = origem;
+    e._destino = destino;
+    e._ano = ano;
+    e._tempo = tempo;
+    e._custo = custo;
 
-    balconia.estradas.push_back(ida);
-
-    Estrada volta;
-    volta._origem = destino;
-    volta._destino = origem;
-    volta._ano = ano;
-    volta._tempo = tempo;
-    volta._custo = custo;
-
-    balconia.estradas.push_back(volta);
+    balconia.estradas.push_back(e);
   }
 
-  balconia.atualizarNumeroEstradas();
-
   BelmannFord(balconia);
+
+  Kruskal(balconia, [](const Estrada& a, const Estrada& b) { return a._ano < b._ano; }, false);
+  Kruskal(balconia, [](const Estrada& a, const Estrada& b) { return a._custo < b._custo; }, true);
 
   return 0;
 }
